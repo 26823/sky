00093db86b1a19d55ab28cd8c19002b5ba879cab
26823 <26823@greijdanus.nl>
2026-09-17 20:20:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+kaboom({
+    width: window.innerWidth,
+    height: window.innerHeight,
 });
+
+loadSprite("bg", "BG.jpg");
+loadSprite("player", "player.png");
+
+scene("game", () => {
+
+    add([
+        sprite("bg"),
+        pos(0, 0),
+        scale(
+            Math.max(
+                width() / 1920,
+                height() / 1080
+            )
+        ),
+        fixed(),
+    ]);
+
+    add([
+        sprite("player"),
+        pos(width() / 2, 200),
+        scale(0.5),
+    ]);
+
+});
+
+go("game");
